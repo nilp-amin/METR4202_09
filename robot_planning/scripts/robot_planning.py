1bbf3e4d86d61a7bdd4ad6a448746fe0a428494a
Justin <justin.kim1@uqconnect.edu.au>
2021-10-07 11:39:38 +1000

    added a bit more to the callback function, need to finish it by friday

--- robot_planning/scripts/robot_planning.py
+++ robot_planning/scripts/robot_planning.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python
 
 import rospy
-import tf
 
-from geometry_msgs.msg import Transform
-from tf2_msgs.msg import TFMessage
 from std_msgs.msg import String
 from std_msgs.msg import Bool
 
@@ -22,9 +19,11 @@ class RobotTrajectory():
         self.angle_sub = rospy.Subscriber("/scara_angles", Float32MultiArray, self.callback_angle)
         self.joint_sub = rospy.Subscriber("/joint_states", JointState, self.callback)
         self.allowable_sub = rospy.Subscriber("/scara_move", Bool, self.boolcall)
-        self.rate = rospy.Rate(1)
+        self.rate = rospy.Rate(0.25)
         
         self.ready = False
+        self.cv_ready = False
+        self.home_config = [2, 2, 2, 2]
         self.current_joint_pos = []
         self.to_move_pos = []
 
@@ -32,6 +31,13 @@ class RobotTrajectory():
     # position, array given by the servos
     def callback(self, data):
         self.current_joint_pos = data.position
+        if self.ready:
+            to_move = np.array(self.to_move_pos)
+            current_pos = np.array(self.current_joint_pos)
+            error = to_move - current_pos
+            if error[0] < 0.01 and error[1] < 0.01 and error[2] < 0.01 and error[3] < 0.01:
+                
+
         self.rate.sleep()
 
     # Checking if the joint angles are
@@ -48,3 +54,9 @@ class RobotTrajectory():
             self.to_move_pos = data.data
 
         self.rate.sleep()
+
+    def run(self):
+        rate.sleep(3)
+        rospy.spin()
+
+    if __name__ == '__main__':
