4faf7e47dc6ec0dcdf5713c311eb9e37be5ab0e4
Nilp Amin <49432005+Nilp-amin@users.noreply.github.com>
2021-10-07 14:04:00 +1000

    Updated robot vision file to be more consitent with message types

--- robot_vision/scripts/robot_vision.py
+++ robot_vision/scripts/robot_vision.py
@@ -7,33 +7,33 @@ from geometry_msgs.msg import Transform
 from tf2_msgs.msg import TFMessage
 from std_msgs.msg import String
 from std_msgs.msg import Bool
-from fiducial_msgs.msg import FiducialTransformArray
+from fiducial_msgs.msg import FiducialTransform
 
 import modern_robotics as mr
 import numpy as np
 
 # TODO: Check if the AruCo markers have stopped moving
 # TODO: Check which AruCo marker transforms are valid (e.g. still on unpicked.)
-# TODO: Check which AruCO marker is on the right side of the table
+# TODO: Check which AruCo marker is on the right side of the table
 # TODO: Publish (x, y, z, theta, phi, psi) realtive to space frame to topic
 
 class RobotVision():
     def __init__(self):
         rospy.init_node("scara_cv", anonymous=True)
-        self.pub = rospy.Publisher("block_transform", FiducialTransformArray, queue_size=10)
-        self.pub = rospy.Publisher("block_ready", String, queue_size=10)
-        self.sub = rospy.Subscriber("/fiducial_transforms", FiducialTransformArray, self.callback)
-        self.sub = rospy.Subscriber("/scara_home", Bool, self.ready_callback)
+        self.block_transform_pub = rospy.Publisher("block_transform", FiducialTransform, queue_size=10)
+        self.block_ready_pub = rospy.Publisher("block_ready", String, queue_size=10)
+        self.fid_transform_sub = rospy.Subscriber("/fiducial_transforms", FiducialTransform, self.callback)
+        self.scara_home_sub = rospy.Subscriber("/scara_home", Bool, self.ready_callback)
         self.listner = tf.TransformListener()
         self.rate = rospy.Rate(1)
 
         self.ready_msg = String() # Used to tell robot_kinematics that transform frame is a valid pickup location 
-        self.tf_msg = FiducialTransformArray() # Used to send the transform frame of block
+        self.tf_msg = FiducialTransform() # Used to send the transform frame of block
         # TODO: Get actual measurements and orientation for this transform
         self.Tbase_cam = np.array([
                                         [1, 0, 0, 0.1],
                                         [0, 1, 0, 0.0],
-                                        [0, 0, 1, 0.5].
+                                        [0, 0, 1, 0.5],
                                         [0, 0, 0, 1.0]
                                   ])
         self.old_distance = 0
@@ -41,26 +41,33 @@ class RobotVision():
         self.robot_ready = False 
         pass
 
-    def self.ready_callback(self, data):
+    def ready_callback(self, data):
         self.robot_ready = data.data
         pass
 
     def callback(self, data):
         # rospy.loginfo(data.transforms) # data.transforms -> list()
-        if (len(data.transforms)) and self.robot_ready:
-            (trans, rot)  = self.listner.lookupTransform("/0", 
-                    f"/fiducial_{data.transforms[0].fiducial_id}", rospy.Time(0))
-            # Check if a block is still moving
-            if not is_moving(trans):
-                (fid_id, trans, rot) = self.find_block_transform(data)
-                self.update_tf_msg(fid_id, trans, rot)
-                self.pub.publish(self.tf_msg)
+        try:
+            if (len(data.transforms)) and self.robot_ready:
+                (trans, rot)  = self.listner.lookupTransform("/0", 
+                        f"/fiducial_{data.transforms[0].fiducial_id}", rospy.Time(0))
+                # Check if a block is still moving
+                if not is_moving(trans):
+                    (fid_id, trans, rot) = self.find_block_transform(data)
+                    self.update_tf_msg(fid_id, trans, rot)
+                    self.pub.publish(self.tf_msg)
+                    pass
+                
+                rospy.loginfo(data.transforms[0].fiducial_id)
+                rospy.loginfo(dist)
                 pass
-            
-            rospy.loginfo(data.transforms[0].fiducial_id)
-            rospy.loginfo(dist)
+            else:
+                pass
+        except tf.LookupException as LE:
+            rospy.loginfo(LE)
             pass
-        else:
+        except tf.ConnectivityException as CE:
+            rospy.loginfo(CE)
             pass
         pass
 
@@ -111,23 +118,50 @@ class RobotVision():
         return (max_dist_id, trans, rot)
         pass
 
-    def run2(self):
-        rospy.sleep(3) # Sleep added so information is not lost when starting up 
-        while not rospy.is_shutdown():
-            try:
-                # /0 is the camera
-                (trans, rot)  = self.listner.lookupTransform("/0", "/fiducial_105", rospy.Time(0))
-                rospy.loginfo(trans)
-                rospy.loginfo(rot)
-                pass
-            except tf.LookupException as LE:
-                rospy.loginfo(LE)
-                pass
-            except tf.ConnectivityException as CE:
-                rospy.loginfo(CE)
-                pass
-            self.rate.sleep()
+    def find_block_transform(self, data)->tuples:
+        fiducials = {}
+        max_dist_id = None
+        max_dist = 0
+        for tf in data.transforms:
+            fid_id = tf.fiducial_id
+            (trans, rot) = self.listner.looupTransform("/0", f"/fiducial_{fid_id}", rospy.Time(0))
+            Rcam_block = self.rotation_matrix(rot)
+            Pcam_block = np.array([
+                                    [trans[0]],
+                                    [trans[1]],
+                                    [trans[2]]
+            ])
+            Tcam_block = np.block([
+                                    [Rcam_block , Pcam_block],
+                                    [0, 0, 0, 1]
+                                  ])
+            Tbase_block = self.Tbase_cam @ Tcam_block
+            curr_dist = marker_distance(trans)
+        pass
 
+    def rotation_matrix(self, rotation)->np.array:
+        # Rotation in radians
+        theta_x = rotation[0]
+        theta_y = rotation[1]
+        theta_z = rotation[2]
+
+        rx = np.array([
+                            [1, 0, 0],
+                            [0, np.cos(theta_x), -np.sin(theta_x)],
+                            [0, np.sin(theta_x), np.cos(theta_x)]
+        ])
+        ry = np.array([
+                            [np.cos(theta_y), 0, np.sin(theta_y)],
+                            [0, 1, 0],
+                            [-np.sin(theta_y), 0, np.cos(theta_y)]
+        ])
+        rz = np.array([
+                            [np.cos(theta_z), -np.sin(theta_z), 0],
+                            [np.sin(theta_z), np.cos(theta_z), 0],
+                            [0, 0, 1]
+        ])
+        return rz @ ry @ rx
+        pass
 
 if __name__ == "__main__":
     try:
