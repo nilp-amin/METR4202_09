a66a837a094aaf3ea8ba81c18c32aa3400649bb8
Your Name <nilpamin2@gmail.com>
2021-10-21 16:53:58 +1000

    Fixed planning code.

--- robot_planning/scripts/robot_planning.py
+++ robot_planning/scripts/robot_planning.py
@@ -13,25 +13,27 @@ class RobotTrajectory():
         rospy.init_node("scara_trajectory", anonymous=True)
         self.desired_joint_state_pub = rospy.Publisher("/desired_joint_states", JointState, queue_size=1)
         self.scara_home_pub = rospy.Publisher("/scara_home", Bool, queue_size = 1)
-        self.joint_sub = rospy.Subscriber("/joint_states", JointState, self.ik_joints_callback, queue_size=1)
+        self.joint_sub = rospy.Subscriber("/scara_angles", Float32MultiArray, self.ik_joints_callback, queue_size=1)
         self.rate = rospy.Rate(0.25)
 
         self.pitch = 8e-3
         self.G = 8.11
         self.prismatic_lower_dist = 30e-3
         self.search_postion = [math.pi/2, 0, 0, 0] 
-        self.wait_time = 2 
+        self.wait_time = 1 
+        self.prismatic_wait_time = 2
 
     def ik_joints_callback(self, joint_angles):
-        pickup_theta_1 = joint_angles[0]
-        pickup_theta_2 = self.prismatic_lower_dist / (self.pitch * self.G) 
-        pickup_theta_3 = joint_angles[1]
-        pickup_theta_4 = joint_angles[2]
+        pickup_theta_1 = joint_angles.data[0]
+        # pickup_theta_2 = self.prismatic_lower_dist / (self.pitch * self.G) 
+        pickup_theta_2 = 2 
+        pickup_theta_3 = joint_angles.data[1]
+        pickup_theta_4 = joint_angles.data[2]
 
-        dropoff_theta_1 = joint_angles[3]
+        dropoff_theta_1 = joint_angles.data[3]
         dropoff_theta_2 = 0
-        dropoff_theta_3 = joint_angles[4]
-        dropoff_theta_4 = joint_angles[5]
+        dropoff_theta_3 = joint_angles.data[4]
+        dropoff_theta_4 = joint_angles.data[5]
 
         # First publish that the robot is at search position 
         scara_home_msg = Bool()
@@ -49,15 +51,17 @@ class RobotTrajectory():
         # Now lower the gripper onto the block
         joint_msg.name = ["joint_2"]
         joint_msg.position = [pickup_theta_2]
-        joint_msg.velocity = [1]
-        rospy.sleep(self.wait_time)
+        joint_msg.velocity = [2]
+        self.desired_joint_state_pub.publish(joint_msg)
+        rospy.sleep(self.prismatic_wait_time)
 
         # TODO: Grab the block
 
         # Move gripper above collision zone
         joint_msg.name = ["joint_2"]
         joint_msg.position = [dropoff_theta_2]
-        joint_msg.velocity = [1]
+        joint_msg.velocity = [2]
+        self.desired_joint_state_pub.publish(joint_msg)
         rospy.sleep(self.wait_time)
 
         # Move robot to drop off zone
@@ -74,7 +78,7 @@ class RobotTrajectory():
         joint_msg.position = self.search_postion 
         joint_msg.velocity = [2, 2, 2]
         self.desired_joint_state_pub.publish(joint_msg)
-        rospy.sleep(self.wait_time)
+        rospy.sleep(self.prismatic_wait_time)
 
         # publish that the robot is in search position
         scara_home_msg.data = True
